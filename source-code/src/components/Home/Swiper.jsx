8efd4d7483afa38521997a2f2eeb74ffef63336e
ashraf <ashrafmohamed1176@gmail.com>
2023-10-03 00:33:01 +0300

    add apis products

--- source-code/src/components/Home/Swiper.jsx
+++ source-code/src/components/Home/Swiper.jsx
@@ -1,8 +1,7 @@
 import Carousel from "react-bootstrap/Carousel";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 // images test
 import image1 from "../../assets/images/f2.jpg";
-import image2 from "../../assets/images/f3.jpg";
 
 const Swiper = () => {
   const [index, setIndex] = useState(0);
@@ -10,36 +9,26 @@ const Swiper = () => {
     setIndex(selectedIndex);
   };
 
+  const [poducts, setPoducts] = useState([]);
+  const api = 'https://database-products.onrender.com/products';
+  useEffect(() => {
+    fetch(api).then((response) => response.json()).then((data) => setPoducts(data))
+  }, []);
+
+
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item style={{ background: "rgba(0,112,255,0.58)", padding: '10px 0px' }} interval={2000}>
-        <div className="flex-center flex-row">
-          <img
-            className="py-3 m-lg-2"
-            style={{ height: "200px", width: "220px" }}
-            src={image1}
-            alt="First slide"
-          />
-          <div>
-            <h2>offer 30%</h2>
-            <p className="">hello product</p>
-          </div>
-        </div>
+        {poducts.map((product) => (
+      <Carousel.Item style={{ background: "rgba(0,112,255,0.58)", padding: '10px 0px' }} interval={2000} key={product.id}>
+            <div className="flex-center flex-row">
+              <img className="py-3 m-lg-2" style={{ height: "200px", width: "220px" }} src={product.image} alt="First slide" />
+              <div>
+                <h2> {product.title.slice(0, 15)} </h2>
+                <p className="">( {product.category} ) </p>
+              </div>
+            </div>
       </Carousel.Item>
-      <Carousel.Item style={{ background: "#999", padding: '10px 0px' }} interval={2000}>
-        <div className="flex-center flex-row ">
-          <img
-            className="py-3 m-lg-2"
-            style={{ height: "200px", width: "220px" }}
-            src={image2}
-            alt="First slide"
-          />
-          <div>
-            <h2>offer 30%</h2>
-            <p className="">hello product</p>
-          </div>
-        </div>
-      </Carousel.Item>
+        ))}
     </Carousel>
   );
 };
--- source-code/src/components/Products/ProductCard.jsx
+++ source-code/src/components/Products/ProductCard.jsx
@@ -1,13 +1,15 @@
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import {useState} from "react";
 
 const ProductCard = (props) => {
+
   return (
-    <Card style={{ width: "13rem" }}>
-      <Card.Img style={{ height: "10rem" }} variant="top" src={props.image} />
+      <>
+      <Card.Img style={{ height: "10rem", width: '10rem', padding: '6px' }} variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ height: "5rem" }}> {props.description} </Card.Text>
+        <Card.Text style={{ height: "1rem" }}>( {props.description} )</Card.Text>
         <div className="stars">
           <i style={{ color: "gold" }} className="fa-solid fa-star"></i>
           <i style={{ color: "gold" }} className="fa-solid fa-star"></i>
@@ -27,7 +29,7 @@ const ProductCard = (props) => {
           </Link>
         </div>
       </Card.Body>
-    </Card>
+</>
   );
 };
 
