2cb89ddc00d12b87708f966f85cc1042bdb5fa2b
ashraf <ashrafmohamed1176@gmail.com>
2023-10-05 21:10:27 +0300

    finsh cards theme

--- source-code/src/components/Products/ProductCard.jsx
+++ source-code/src/components/Products/ProductCard.jsx
@@ -1,12 +1,20 @@
+import { FaStar } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
 const ProductCard = (props) => {
   return (
     <>
       <img className="product-image" src={props.image} alt="product" />
       <div className="card-content-body">
-        <div className="card-title">
-          {props.title} ({props.category})
+        <div className="card-title">{props.title}</div>
+        <div className="card-category">({props.category})</div>
+        <div className="icons-control">
+          <div className="card-rate flex-center">
+            {props.rate} {<FaStar />}
+          </div>
+          <div className="add-product-cart flex-center">
+            <FaCartShopping />
+          </div>
         </div>
-        <div className="icons-control"></div>
       </div>
     </>
   );
--- source-code/src/pages/product/Products.jsx
+++ source-code/src/pages/product/Products.jsx
@@ -11,7 +11,7 @@ export default function Products() {
   useEffect(() => {
     fetch(api)
       .then((response) => response.json())
-      .then((data) => setProducts(data.slice(0, 15)));
+      .then((data) => setProducts(data.slice(0, 12)));
   }, []);
 
   return (
@@ -27,10 +27,25 @@ export default function Products() {
         >
           {products.map((product) => (
             <div className={"product-card"} key={product.id}>
+              <svg
+                className="hart"
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="24"
+                viewBox="0 0 16 14"
+                fill="none"
+              >
+                <path
+                  d="M4.5 1C2.5673 1 1 2.49112 1 4.33081C1 5.81587 1.6125 9.34047 7.6416 12.9034C7.7496 12.9666 7.87358 13 8 13C8.12642 13 8.2504 12.9666 8.3584 12.9034C14.3875 9.34047 15 5.81587 15 4.33081C15 2.49112 13.4327 1 11.5 1C9.5673 1 8 3.01867 8 3.01867C8 3.01867 6.4327 1 4.5 1Z"
+                  stroke="black" fill="red"
+                  strokeLinecap="round"
+                />
+              </svg>
               <ProductCard
                 image={product.image}
                 title={product.title.slice(0, 10)}
-                category={product.category}
+                category={product.title.slice(0, 10)}
+                rate={product.rating.rate}
               />
             </div>
           ))}
