aa196130fed6990f58d55c57840ed3757a985b71
ashraf <ashrafmohamed1176@gmail.com>
2023-10-05 17:32:17 +0300

    modified products card

--- source-code/src/components/Products/ProductCard.jsx
+++ source-code/src/components/Products/ProductCard.jsx
@@ -1,37 +1,14 @@
-import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import {useState} from "react";
-
 const ProductCard = (props) => {
-
-  
-
   return (
-      <>
-      <Card.Img style={{ height: "10rem", width: '10rem', padding: '6px' }} variant="top" src={props.image} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ height: "1rem" }}>( {props.description} )</Card.Text>
-        <div className="stars">
-          <i style={{ color: "gold" }} className="fa-solid fa-star"></i>
-          <i style={{ color: "gold" }} className="fa-solid fa-star"></i>
-          <i style={{ color: "gold" }} className="fa-solid fa-star"></i>
-          <i style={{ color: "gold" }} className="fa-solid fa-star"></i>
-          <i style={{ color: "gold" }} className="fa-solid fa-star"></i>
-          <Link
-            style={{
-              marginLeft: "30px",
-              textDecoration: "none",
-              color: "red",
-              fontSize: "30px",
-            }}
-            className="icon"
-          >
-            <i className="fa-solid fa-store"> </i>
-          </Link>
+    <>
+      <img className="product-image" src={props.image} alt="product" />
+      <div className="card-content-body">
+        <div className="card-title">
+          {props.title} ({props.category})
         </div>
-      </Card.Body>
-</>
+        <div className="icons-control"></div>
+      </div>
+    </>
   );
 };
 
--- source-code/src/pages/product/Products.jsx
+++ source-code/src/pages/product/Products.jsx
@@ -1,28 +1,41 @@
-// import style
-import "./Products.css";
 import ProductCard from "../../components/Products/ProductCard";
-import {useEffect, useState} from "react";
-import Card from "react-bootstrap/Card";
+import { useEffect, useState } from "react";
+import "./Products.css";
+import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Products() {
-    const [products, setProducts] = useState([]);
-    const api = 'https://database-products.onrender.com/products'
-    useEffect(() => {
-        fetch(api).then((response) => response.json()).then((data) => setProducts(data))
-    }, []);
+  const [products, setProducts] = useState([]);
+
+  const api = "https://database-products.onrender.com/products";
+  useEffect(() => {
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => setProducts(data.slice(0, 15)));
+  }, []);
 
   return (
-    <>
-      <div id="product" className="product">
-        <h1 className={'main-title'}>Product</h1>
-        <div style={{ gap: "20px" }} className="imgs col-lg-16 ">s
-            {products.map(product => (
-                <Card style={{width: '14rem'}} className={'d-flex align-items-center'} key={product.id}>
-                <ProductCard image={product.image} title={product.title.slice(0, 10)} description={product.category} />
-                </Card>
-            ))}
+    <div className="product master-producs-home pb-5">
+      <Container fluid className="pt-5">
+        <h1 className="main-title d-flex justify-content-between align-items-center">
+          {"-"}Product
+          <Link className="main-title-content">show more</Link>
+        </h1>
+        <div
+          style={{ gap: "20px" }}
+          className="products-container col-lg-16 pt-5"
+        >
+          {products.map((product) => (
+            <div className={"product-card"} key={product.id}>
+              <ProductCard
+                image={product.image}
+                title={product.title.slice(0, 10)}
+                category={product.category}
+              />
+            </div>
+          ))}
         </div>
-      </div>
-    </>
+      </Container>
+    </div>
   );
 }
