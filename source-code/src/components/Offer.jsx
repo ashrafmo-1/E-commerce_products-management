b781a735d41ed7f74d90a18bc349cf28be244717
ashraf <ashrafmohamed1176@gmail.com>
2023-10-10 23:27:40 +0300

    edit show product error on api link

--- source-code/src/components/Offer.jsx
+++ source-code/src/components/Offer.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Offer = (props) => {
-    return (
-        <div className={'sect-offers text-center py-5 mt-4'}>
-            <h2>get big offers</h2>
-            <p>black friday offers on accessorys and elctronic</p>
-            <button className={'btn btn-outline-danger'}>get now</button>
-        </div>
-    )
-}
+  return (
+    <div className={"sect-offers text-center py-5 mt-4"}>
+      <h2>get big offers</h2>
+      <p>black friday offers on accessorys and elctronic</p>
+      <button className={"btn btn-outline-danger"}>get now</button>
+    </div>
+  );
+};
 
 export default Offer;
