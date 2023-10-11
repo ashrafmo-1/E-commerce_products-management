28abc96c3b59b454365e43bd8419f3bd77662ea6
ashraf <ashrafmohamed1176@gmail.com>
2023-10-11 06:55:38 +0300

    add show products section and style end

--- source-code/src/components/ShowProduct.jsx
+++ source-code/src/components/ShowProduct.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import "./showProduct.css";
+import NavgationBar from "./NavgationBar";
 
 const ShowProduct = () => {
   const { productInformation } = useParams();
@@ -14,18 +17,28 @@ const ShowProduct = () => {
   }, []);
 
   return (
-    <section className="show-products">
-      <div className="info">
-        <img src={product.image} alt="" />
-        <div className="show-product-content">
-          <span className="catigory">{product.category}</span>
-          <h2 className="title">{product.title}</h2>
-          <p className="description"> {product.description} </p>
-          <p className="price">$999</p>
-          <p className="adding">add to cart</p>
+    <>
+      <NavgationBar />
+      <Container className="show-products flex-center">
+        <div className="info flex-center gap-5 w-75 h-75">
+          <img src={product.image} alt="" className="" />
+          <div className="show-product-content">
+            <span className="catigory">{product.category}</span>
+            <h2 className="title mb-4">{product.title}</h2>
+            <p className="description mb-3"> {product.description} </p>
+            <div className="d-flex gap-5 align-items-center">
+              <div className="quantity flex-center gap-3">
+                <button className="btn btn-success">+</button>
+                <span>1</span>
+                <button className="btn btn-danger">-</button>
+              </div>
+              <p className="price flex-center">$999</p>
+            </div>
+            <p className="add-to-cart btn mt-4">add to cart</p>
+          </div>
         </div>
-      </div>
-    </section>
+      </Container>
+    </>
   );
 };
 
